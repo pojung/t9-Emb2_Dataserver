919f2ee23e22107846fce00727e84202d84e3a4b
waveUbuntu <hiiwave@gmail.com>
2015-04-21 01:39:27 +0800

    ok

--- dataserver.js
+++ dataserver.js
@@ -2,21 +2,17 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
 var port = process.env.PORT || 5000;
+
 server.listen(port, function() {
   console.log("Express server listening on port %d", server.address().port);
 });
 
-// io.set('transports', [ 'jsonp-polling' ]);
-
+// set up mongoDB database
 var mongoose = require( 'mongoose' );
-// var mongodbUrl = (process.env.MONGOLAB_URI)? process.env.MONGOLAB_URI : 'mongodb://localhost/test';
 var mongodbUrl = (process.env.MONGOLAB_URI)? process.env.MONGOLAB_URI : 'mongodb://heroku_app35998051:nvjupt69fjpud7br66se29r23f@ds035167.mongolab.com:35167/heroku_app35998051';
+  // var mongodbUrl = (process.env.MONGOLAB_URI)? process.env.MONGOLAB_URI : 'mongodb://localhost/test';  // for using local database
 mongoose.connect(mongodbUrl);
-// mongoose.connect( 'mongodb://localhost/test' );  // for local test
-// mongoose.connect(process.env.MONGOLAB_URI);  // for heroku
-// mongo ds035167.mongolab.com:35167/heroku_app35998051 -u heroku_app35998051 -p nvjupt69fjpud7br66se29r23f
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -32,7 +28,7 @@ db.once('open', function (callback) {
   });
   var Lab2Collection = mongoose.model('Lab2Collection', Lab2Schema);
 
-  io.on('connection', function(socket) {  // connection for monitor.js only
+  io.on('connection', function(socket) {  // connection setup for monitor.js
     console.log('A monitor connected');
     var historyStream = Lab2Collection.find().sort({_id : -1}).limit(10).stream();
     historyStream.on('data', function(pkt) {
