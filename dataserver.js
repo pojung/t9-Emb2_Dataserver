f22ab21354e4e9efec07312c9ea23c71a0fef1fe
waveUbuntu <hiiwave@gmail.com>
2015-04-21 00:18:34 +0800

    not know why many requests happened when refresh page

--- dataserver.js
+++ dataserver.js
@@ -2,44 +2,72 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var mongoose = require( 'mongoose' );
-
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
   console.log("Express server listening on port %d", server.address().port);
 });
 
-app.get('/', function(req, res) {
-  res.send('Try /socket.html');
-});
+var mongoose = require( 'mongoose' );
+// mongoose.connect( 'mongodb://localhost/test' );
+mongoose.connect(process.env.MONGOLAB_URI);
 
-app.get('/monitor.html', function(req, res, next) {
-  io.on('connection', function(socket) {
-    console.log('A monitor connected');
-    setInterval(function() {
-      socket.emit('date', {'date': new Date()});
-    }, 40);  
-    socket.on('disconnect', function() {
-      console.log('monitor disconnected');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("Database open");
+  var Lab2Schema = mongoose.Schema({
+    date: Date,
+    noise: Number,
+    temparature: Number,
+    humidity: Number,
+    lat: Number,
+    lng: Number
+  });
+  var Lab2Collection = mongoose.model('Lab2Collection', Lab2Schema);
+
+  app.get('/', function(req, res) {
+    res.send('Use /feed.html to feed random data, and /monitor.html to observe it');
+  });
+
+  app.get('/monitor.html', function(req, res, next) {
+    io.on('connection', function(socket) {
+      console.log('A monitor connected');
+      var historyStream = Lab2Collection.find().sort({_id : -1}).limit(10).stream();
+      historyStream.on('data', function(pkt) {
+        socket.emit('historyPkt', pkt);  
+      })
+      setInterval(function() {
+        socket.emit('date', {'date': new Date()});
+      }, 40);  
+      socket.on('disconnect', function() {
+        console.log('monitor disconnected');
+      });
     });
+    next();
   });
-  next();
+
+  app.post('/feed', function(req, res) {
+    // console.log('Got data fed!');
+    var post_request_body = '';
+    req.on('data', function (data) {
+       post_request_body += data;
+    });
+    req.on('end', function (data) {
+      // console.log('Send event:newData to all clients');
+      var pkt = JSON.parse(post_request_body)
+      var lab2doc = new Lab2Collection(pkt);
+      lab2doc.save(function(err, lab2doc) {
+        if (err)  return console.error(err);
+        console.log("SAVE a document");
+      });
+      io.sockets.emit('newPkt', pkt);  
+    });
+    res.send('Server GOT your data!');
+  });
+
+  app.use(express.static('public'));
 });
 
-app.post('/feed', function(req, res) {
-  // console.log('Got data fed!');
-  var post_request_body = '';
-  req.on('data', function (data) {
-     post_request_body += data;
-  });
-  req.on('end', function (data) {
-    // console.log('Send event:newData to all clients');
-    io.sockets.emit('newData', JSON.parse(post_request_body));  
-  });
-  res.send('Server GOT your data!');
-});
-
-app.use(express.static('public'));
 
 
 // mongo ds035167.mongolab.com:35167/heroku_app35998051 -u heroku_app35998051 -p nvjupt69fjpud7br66se29r23f
---
+++ public/monitor.js
@@ -0,0 +1,15 @@
+var socket = io.connect();
+
+socket.on('date', function(data) {
+  $('#date').text(data.date);
+});
+
+socket.on('newPkt', function(pkt) {
+  var str = pkt.date + '\n' + pkt.noise + '\n' + pkt.temparature + '\n' + pkt.humidity + '\n' + pkt.lat + '\n' + pkt.lng;
+  $('#newData').text(str);
+});
+
+socket.on('historyPkt', function(pkt) {
+  var str = pkt.date + '\n' + pkt.noise + '\n' + pkt.temparature + '\n' + pkt.humidity + '\n' + pkt.lat + '\n' + pkt.lng;
+  console.log("History data got: " + str);
+});
