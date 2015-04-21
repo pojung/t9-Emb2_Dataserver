e7a8b0cb5fd63fae85b20d4904cd096b28b464f1
waveUbuntu <hiiwave@gmail.com>
2015-04-21 13:23:30 +0800

    display history to monitor.js at start

--- public/monitor.js
+++ public/monitor.js
@@ -19,7 +19,7 @@ socket.on('countDb', function(count) {
 });
 
 socket.on('historyPkt', function(pkt) {  
-  var str = pkt.date + '\n' + pkt.noise + '\n' + pkt.temparature + '\n' + pkt.humidity + '\n' + pkt.lat + '\n' + pkt.lng;
+  var str = pkt.date + '<br>' + pkt.noise + '<br>' + pkt.temparature + '<br>' + pkt.humidity + '<br>' + pkt.lat + '<br>' + pkt.lng;
   $('#newData').html(str);
   console.log("History data got: " + str);
 });
